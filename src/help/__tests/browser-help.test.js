5be73f7683c0b3e98d4a0b06649c63d3dbc45310
Marc Nuri <marc@marcnuri.com>
2020-08-23 20:39:44 +0200

    🧪: Tests for Help module

---
+++ src/help/__tests/browser-help.test.js
@@ -0,0 +1,62 @@
+/*
+   Copyright 2019 Marc Nuri San Felix
+
+   Licensed under the Apache License, Version 2.0 (the "License");
+   you may not use this file except in compliance with the License.
+   You may obtain a copy of the License at
+
+       http://www.apache.org/licenses/LICENSE-2.0
+
+   Unless required by applicable law or agreed to in writing, software
+   distributed under the License is distributed on an "AS IS" BASIS,
+   WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+   See the License for the specific language governing permissions and
+   limitations under the License.
+ */
+const {fireEvent} = require('@testing-library/dom');
+
+const mockDOM = () => {
+  document.body.innerHTML = '';
+  const $root = document.createElement('div');
+  $root.innerHTML = '<div class="help-root"></div>';
+  document.body.append($root);
+};
+
+describe('Help in Browser test suite', () => {
+  let mockIpcRenderer;
+  beforeEach(() => {
+    mockIpcRenderer = {
+      send: jest.fn()
+    };
+    window.preact = require('preact');
+    window.preactHooks = require('preact/hooks');
+    window.htm = require('htm');
+    window.APP_EVENTS = {closeDialog: 'close the dialog'};
+    window.docs = {
+      'Setup.md': '<span>Setup guide</span>',
+      'Keyboard-shortcuts.md': '<span>Keyboard shortcuts</span>',
+      'Troubleshooting.md': '<span>Troubleshooting</span>'
+    };
+    window.ipcRenderer = mockIpcRenderer;
+    mockDOM();
+    jest.isolateModules(() => {
+      require('../browser-help');
+    });
+  });
+  test('render, should render all documents', () => {
+    // Then
+    expect(document.querySelector('.toc-container').innerHTML)
+      .toMatch(/Table of Contents/);
+    expect(document.querySelector('.documents-container').innerHTML)
+      .toMatch(/Setup guide.+<span>Keyboard shortcuts.+Troubleshooting/);
+  });
+  describe('Main Button events', () => {
+    test('Close should send close dialog event', () => {
+      // When
+      fireEvent.click(document.querySelector('.button.is-link.is-light'));
+      // Then
+      expect(mockIpcRenderer.send).toHaveBeenCalledTimes(1);
+      expect(mockIpcRenderer.send).toHaveBeenCalledWith('close the dialog');
+    });
+  });
+});
