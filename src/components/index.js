5af563f13884bee610c7619ffe5f3e30843ea659
Marc Nuri <marc@marcnuri.com>
2022-09-28 07:09:33 +0200

    🧹 remove unneeded style component tools
    Signed-off-by: Marc Nuri <marc@marcnuri.com>

--- src/components/index.js
+++ src/components/index.js
@@ -1,7 +1,3 @@
 module.exports = {
-  addStylesheet: require('./styles').addStylesheet,
-  bulma: require('./styles').bulma,
-  chromeTabs: require('./styles').chromeTabs,
-  fontAwesome: require('./styles').fontAwesome,
   topBar: require('./top-bar').topBar
 };
