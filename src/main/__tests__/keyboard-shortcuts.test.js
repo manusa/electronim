2d87c551a454541cd05bc4d702fd391c0492a30e
Marc Nuri <marc@marcnuri.com>
2022-11-09 09:04:02 +0100

    🧪 extract tests for global listeners
    Signed-off-by: Marc Nuri <marc@marcnuri.com>

--- src/main/__tests__/keyboard-shortcuts.test.js
+++ src/main/__tests__/keyboard-shortcuts.test.js
@@ -13,7 +13,7 @@
    See the License for the specific language governing permissions and
    limitations under the License.
  */
-describe('Global Keyboard Shortcuts module test suite', () => {
+describe('Main :: Global Keyboard Shortcuts module test suite', () => {
   let electron;
   beforeEach(() => {
     jest.resetModules();
