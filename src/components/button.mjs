e847ab13bdb3a5d02164bcfbf97afa24c9bee3df
Marc Nuri <marc@marcnuri.com>
2025-08-31 05:36:08 +0200

    minor typos

--- src/components/button.mjs
+++ src/components/button.mjs
@@ -1,5 +1,5 @@
 /*
-   Copyright 2022 Marc Nuri San Felix
+   Copyright 2025 Marc Nuri San Felix
 
    Licensed under the Apache License, Version 2.0 (the "License");
    you may not use this file except in compliance with the License.
